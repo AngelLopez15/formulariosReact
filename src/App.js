9af7119bd2daac86a0c8f4a9f373ab5807c85b66
Angel Lopez <a.o.lopez.cruz@gmail.com>
2022-05-25 23:43:27 -0500

    componentes controlados y no controlados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   // En react existe el concepto de componente controlados y los componentes mantienene el estado por defecto
@@ -26,19 +26,38 @@ function App() {
     setNum(e.target.value.length);
   }
 
+  const email = useRef();
+
   const submit = (e) => {
     // Con e.preventDefault hacemos que el comportamiento por defecto no sucesa
     // lo siguiente despues de esa linea podemos hacer lo que queramos
     e.preventDefault();
     // alert("Formulario enviado");
     // console.log(e); // de esta manera podemos entrar a a ver todas las propiedades del evento
-    console.log(e.target);
+    console.log('e.target: ', e.target);
+    console.log('e.target: ', email.current.value);
   }
+
+  // Componentes no controlados
+  // Los componentes no controlados son aquellos que sus eventos no dependen del estado de react y ese evento se le
+  // relega a que lo maneje el DOM.
+
+  // Componentes controlados
+  // Los componentes controlados son aquellos que sus eventos son manejados a traves de un estado de react
+
+  // para capturar un elemento en react se hace atraves de referencias
+  // para acceder al valor de un input hay que declarar un "ref"
+  // const email = useRef();
 
   return (
     <div>
       <form onSubmit={ submit }>
-        <input type="email" defaultValue="angel@correo.com" />
+        {/* Este input seria un componente no controlado por que es una referencia la que lo controla, delegamos el evento al DOM */}
+        <input type="email" defaultValue="angel@correo.com" ref={ email } />
+        {/* Este otro input es un componente controlado por que el evento es manejado por un estado de react el onChange que ejecuta una funcion (changeEmail) que meneja el estado*/}
+        {/* este seria el estado que maneja el input -> const [mail, setMail] = useState("") */}
+        {/* y esta seria la funcion -> const changeEmail = (e) => setMail(e.target.value) */}
+        {/* <input type="email" value="mail" onChange={ changeEmail } /> */}
         <input type="password" placeholder="Contraseña" required onChange={ countCharacter} />
         {/* <textarea name="about" id="about" cols="30" rows="10" defaultValue="Hola Mundo" /> */}
         {/* <select name="country" id="country" defaultValue="mx" required>
