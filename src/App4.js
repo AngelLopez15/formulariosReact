b737bcc01a238a686a39dcb184e3fe22a4575fe3
Angel Lopez <a.o.lopez.cruz@gmail.com>
2022-05-31 01:10:16 -0500

    Formulario con react hook form

--- src/App4.js
+++ src/App4.js
@@ -8,11 +8,31 @@ const App4 = () => {
   // El register se debe de poner en el input dentro de llaves y con el spred operator
   // La funcion handleSubmit nos ayudara a manipular el evento submit del formulario
   // handleSubmit recibe como parametro otra funcion.
-  const { register, handleSubmit } = useForm()
+  // La watch es un observador que estara vigilando los cambios del formulario. 
+  // El watch solo puede vigilar algo que este registrado
+  // recibe como parametro el campo registrado que queremos vigilar watch("email")
+  // formState hace referencia al estado del formulario y tiene como valor un objeto
+  // dentro del objeto hay un atributo que se llama errors
+  // en errors se pueden almacenar todos los errores que el formulario genere
+  const { 
+    register, 
+    handleSubmit, 
+    watch, 
+    formState: {
+      errors,
+    },
+  } = useForm()
 
-  const submit = () => {
+  const submit = (data) => {
     console.log('Hola mundo');
-  } 
+  }
+
+  console.log('errors', errors);
+
+  // En el register como segundo parametro van todas las validaciones que queremos que tenga
+  // ese campo. y se ponen dentro de un objeto
+  // { required: true} si queremos que en el error mande un mensaje. En lugar de true podemos poner
+  // un string con el mensaje
 
   return (
     <div>
@@ -21,27 +41,47 @@ const App4 = () => {
           <label htmlFor="email">
             Correo eléctronico
             <input
-              { ...register("email") }
+              { ...register("email", { required: "El correo es obligatorio"}) }
               type="email"
               name="email" 
               id="email"
               defaultValue=""
             />
           </label>
+          {
+            // usando el operador conditional chaining para validar las propiedades del objeto
+            errors?.email && errors?.email?.message
+          }
         </div>
         <div>
           <label htmlFor="password">
             Contraseña
             <input
-              { ...register("password") }
+              { ...register("password", {
+                required: "La contraseña es obligatoria",
+                minLength: 8,
+                maxLength: 10,
+                pattern: /^[A-Za-z]+$/i,
+                validate: dato => {
+                  console.log('Este es el dato del validate', dato);
+                },
+              }) }
               type="password"
               name="password" 
               id="password"
               defaultValue=""
             />
           </label>
+          {
+            errors?.password &&
+            <span>
+              { errors?.password?.message }
+            </span>
+          }
         </div>
-        <div></div>
+        <div>
+          <input type="submit" value="ingresar" />
+        </div>
       </form>
     </div>
   )
